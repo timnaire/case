3d48fdddf48d512bf0616446f719d692fe4cfecc
Timmy <timmydonaire@gmail.com>
2018-12-23 03:10:09 +0800

    registration complete, can saved now to datastore.

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, session, redirect
+from config import SECRET_KEY
 
 from models.lawyer import Lawyer
 
+
 app = Flask(__name__)
+app.secret_key = SECRET_KEY
 
 @app.route('/')
 def login():
-    return render_template('index.html',title='Login')
+    return render_template('login.html',title='Login')
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register():
+    if request.method == 'POST':
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        specialize = request.form.get('specialize')
+        bar_number = request.form.get('bar_number')
+
+        lawyer = Lawyer.save(first_name=first_name,last_name=last_name,email=email,phone=phone,specialize=specialize,bar_number=bar_number,password='')
+        if lawyer:
+            return redirect(url_for('verify'))
+        else:
+            return redirect(
+                url_for('register',
+                    err=1, m="Something went wrong please try again."))
+
     return render_template('register.html',title='Registration')
+
+@app.route('/verify')
+def verify():
+    return render_template('verify.html', title='Verifying details')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models/lawyer.py
+++ models/lawyer.py
@@ -4,10 +4,33 @@ from passlib.hash import pbkdf2_sha256
 class Lawyer(ndb.Model):
     first_name = ndb.StringProperty()
     last_name = ndb.StringProperty()
+    email = ndb.TextProperty()
     phone = ndb.StringProperty()
-    email = ndb.StringProperty()
-    specialized = ndb.StringProperty()
-    password = ndb.StringProperty()
+    specialize = ndb.StringProperty()
     bar_number = ndb.StringProperty()
+    password = ndb.StringProperty()
     created = ndb.DateTimeProperty(auto_now_add=True)
     updated = ndb.DateTimeProperty(auto_now=True)
+
+    @classmethod
+    def save(cls, *args, **kwargs):
+        uid = kwargs.get('id')
+        lawyer = cls.get_by_id(int(uid)) if uid else cls()
+
+        if kwargs.get('first_name'):
+            lawyer.first_name = kwargs.get('first_name')
+        if kwargs.get('last_name'):
+            lawyer.last_name = kwargs.get('last_name')
+        if kwargs.get('email'):
+            lawyer.email = kwargs.get('email')
+        if kwargs.get('phone'):
+            lawyer.phone = kwargs.get('phone')
+        if kwargs.get('specialize'):
+            lawyer.specialize = kwargs.get('specialize')
+        if kwargs.get('bar_number'):
+            lawyer.bar_number = kwargs.get('bar_number')
+        if kwargs.get('password'):
+            lawyer.password = pbkdf2_sha256.hash(kwargs.get('password'))
+
+        lawyer.put()
+        return lawyer
