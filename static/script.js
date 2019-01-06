8a92cf99a09f4596ef61a7589092aba1db073675
Timmy <timmydonaire@gmail.com>
2019-01-06 14:15:08 +0800

    not using ajax for the mean time

--- static/script.js
+++ static/script.js
@@ -1,33 +1,38 @@
-$('#btnRegister').click(function(e) {
-    var first_name = $('#first_name').val();
-    var last_name = $('#last_name').val();
-    var email = $('#email').val();
-    var phone = $('#phone').val();
-    var office = $('#office').val();
-    var specialize = $('#specialize').val();
-    var bar_number = $('#bar_number').val();
+// $('#btnRegister').click(function(e) {
+//     var first_name = $('#first_name').val();
+//     var last_name = $('#last_name').val();
+//     var email = $('#email').val();
+//     var phone = $('#phone').val();
+//     var office = $('#office').val();
+//     var law_practice = $('#law_practice').val();
+//     var bar_number = $('#bar_number').val();
 
-    var sendInfo = {
-        first_name : first_name,
-        last_name : last_name,
-        email : email,
-        phone : phone,
-        office : office,
-        specialize : specialize,
-        bar_number : bar_number
-    }
+//     var sendInfo = {
+//         first_name : first_name,
+//         last_name : last_name,
+//         email : email,
+//         phone : phone,
+//         office : office,
+//         law_practice : law_practice,
+//         bar_number : bar_number
+//     }
 
-    $.ajax({
-        url: '/attorneys',
-        type: 'POST',
-        dataType: 'json',
-        contentType: 'application/json',
-        data: JSON.stringify(sendInfo),
-        success: function(response) {
-            console.log(response);
-        },
-        error: function(error) {
-            console.log(error);
-        }
-    });
-});
+//     $.post("/signup/attorneys",JSON.stringify(sendInfo),function(result){
+//         console.log(result);
+//     });
+    
+    // $.ajax({
+    //     url: '/signup/attorneys',
+    //     type: 'POST',
+    //     dataType: 'json',
+    //     contentType: 'application/json; charset-utf-8',
+    //     data: JSON.stringify(sendInfo),
+    //     success: function(response) {
+    //         console.log(response);
+    //     },
+    //     error: function(error) {
+    //         console.log(error);
+    //     }
+    // });
+    // e.preventDefault();
+// });
